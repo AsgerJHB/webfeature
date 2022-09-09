780cda5cfc2c5616a8672af9980dc75e9fb50f90
Asger <asge0729@stud.kea.dk>
2022-09-09 11:44:57 +0200

    parallax

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import "./style.css";
 /* Import af Motion One bibliotek */
 import { animate, stagger, inView, scroll, timeline } from "motion"
+
+document.querySelectorAll(".parallaxcontainer").forEach((parallaxcontainer) => {
+  const elementderskalparallaxes = parallaxcontainer.querySelector("img");
+  scroll(animate(elementderskalparallaxes, { y: [-500, 300] }), {
+    target: elementderskalparallaxes,
+  });
+});
