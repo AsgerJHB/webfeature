8ac029db174996c06eae0f8a10832ed34bf2ccd6
Julie Jakobsen <juliehage@live.dk>
2022-09-12 13:32:06 +0200

    fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontSize: {
+      xs: "4.236rem",
+      sm: "6.854rem",
+      lg: "11.089rem",
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
