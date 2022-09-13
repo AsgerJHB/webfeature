32bcfcad6f13e9aadc6f7a39304ae7d39e94fa55
Louise Traberg <louisetrab@gmail.com>
2022-09-13 12:36:05 +0200

    changes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import "./style.css";
 /* Import af Motion One bibliotek */
 import { animate, stagger, inView, scroll, timeline } from "motion";
 
+const lillevideo = document.querySelector("#videofil");
+lillevideo.pause();
+
+scroll(
+  ({ y }) => {
+    if (!lillevideo.readyState) return;
+    lillevideo.currentTime = lillevideo.duration * y.progress;
+  },
+  {
+    target: document.querySelector(".videoarticle"),
+    offset: ["-100vh", "end end"],
+  }
+);
+
 scroll(animate(".progress-bar", { scaleX: [0, 1] }));
 
 document.querySelectorAll(".parallaxcontainer").forEach((parallaxcontainer) => {
@@ -17,20 +31,6 @@ document.querySelectorAll(".polaroid").forEach((polaroid) => {
     target: elementderskalvises,
   });
 });
-
-const lillevideo = document.querySelector("#videofil");
-lillevideo.pause();
-
-scroll(
-  ({ y }) => {
-    if (!lillevideo.readyState) return;
-    lillevideo.currentTime = lillevideo.duration * y.progress;
-  },
-  {
-    target: document.querySelector(".videoarticle"),
-    offset: ["-100vh", "end end"],
-  }
-);
 
 inView(".polaroid_section", ({ target }) => {
   animate(
